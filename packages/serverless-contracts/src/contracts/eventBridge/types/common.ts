78f91fa5c8d1f06b7ee9181615dc9f50ae9fdb99
Sc0ra <axelf@theodo.fr>
2024-07-24 16:11:30 +0200

    v0.33.0-alpha.2

--- packages/serverless-contracts/src/contracts/eventBridge/types/common.ts
+++ packages/serverless-contracts/src/contracts/eventBridge/types/common.ts
@@ -1,4 +1,3 @@
-import { EventBridgeEvent as AwsEventBridgeEvent } from 'aws-lambda';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 
 import { EventBridgeContract } from '../eventBridgeContract';
@@ -8,5 +7,6 @@ export type EventBridgePayloadType<Contract extends EventBridgeContract> =
     ? FromSchema<Contract['payloadSchema']>
     : void;
 
-export type EventBridgeEvent<Contract extends EventBridgeContract> =
-  AwsEventBridgeEvent<Contract['eventType'], EventBridgePayloadType<Contract>>;
+export type EventBridgePayloadUnionType<
+  Contracts extends EventBridgeContract[],
+> = EventBridgePayloadType<Contracts[number]>;
